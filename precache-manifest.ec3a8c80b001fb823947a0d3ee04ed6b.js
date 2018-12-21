7a2a07cee6604c62d619112cdb9984fbd436f852
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-21 01:21:03 +0000

    Deploy ykarim/openmrs-contrib-gci-site to github.com/ykarim/openmrs-contrib-gci-site.git:gh-pages

---
+++ precache-manifest.ec3a8c80b001fb823947a0d3ee04ed6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fdc4828b1cd79ed7320",
+    "url": "/openmrs-contrib-gci-site/static/css/main.897e6724.chunk.css"
+  },
+  {
+    "revision": "6fdc4828b1cd79ed7320",
+    "url": "/openmrs-contrib-gci-site/static/js/main.6fdc4828.chunk.js"
+  },
+  {
+    "revision": "16c8866f5029132d614b",
+    "url": "/openmrs-contrib-gci-site/static/js/1.16c8866f.chunk.js"
+  },
+  {
+    "revision": "3b93671f5ddfa3d4866f",
+    "url": "/openmrs-contrib-gci-site/static/js/runtime~main.3b93671f.js"
+  },
+  {
+    "revision": "5d84dd10550aa7ea21caebc37fbf3c23",
+    "url": "/openmrs-contrib-gci-site/index.html"
+  }
+];
