c9e76e0ceea19045cc530ce8a7a93f94f2e1e03d
Yusuf Karim <ykarim250@gmail.com>
2018-12-20 19:12:56 -0500

    ➕ Created basic homepage with Bulma
    CSS files now converted to scss
    Bulma imported in index.scss

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <section className="section">
+            <div className="container">
+                <h1 className="title">
+                    Hello World
+                </h1>
+                <p className="subtitle">
+                    My first website with <strong>Bulma & React</strong>!
+                </p>
+            </div>
+        </section>
     );
-  }
 }
 
 export default App;
